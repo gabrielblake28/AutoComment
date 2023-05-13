98bc2cdd54d801993d750231bbdcd9d544a6fa7d
Gabriel Blake <gabriel.blake28@gmail.com>
2023-05-12 21:30:12 -0700

    refactor

--- src/extension.ts
+++ src/extension.ts
@@ -3,10 +3,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 import { config } from "dotenv";
-import { generateComment, generateExplanation } from "./commands/commands";
-import { ExplanationPanel } from "./explanationPanel";
-import { SidebarProvider } from "./sidebarProvider";
 import { getNonce } from "./getNonce";
+import { Configuration, OpenAIApi } from "openai";
+import { GenerateCommentCommand } from "./services/commandService/impl/GenerateComment/GenerateCommentCommand";
+import { GenerateExplanationCommand } from "./services/commandService/impl/GenerateExplanation/GenerateExplanationCommand";
+import { ExplanationViewProvider } from "./services/viewProviders/ExplanationViewProvider";
 config();
 
 // This method is called when your extension is activated
@@ -20,46 +21,63 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
 
-  const explanationProvider = new ExplanationViewProvider(context.extensionUri);
+  console.log('Activating extension "CodeSense ai"');
 
-  // let expalanationViewCmd = vscode.commands.registerCommand(
-  //   "firstextension.explanationview",
-  //   () => {
-  //     vscode.window.registerWebviewViewProvider(
-  //       ExplanationViewProvider.viewType,
-  //       explanationProvider,
-  //       {
-  //         webviewOptions: { retainContextWhenHidden: true },
-  //       }
-  //     );
-  //   }
-  // );
+  const configuration = new Configuration({
+    organization: "org-dEtIga6fE6tm2FkqJmKYh9Qc",
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+  const provider = new ExplanationViewProvider(context.extensionUri);
 
-  let autoExplanation = vscode.commands.registerCommand(
+  let generateExplanation = vscode.commands.registerCommand(
     "firstextension.generateexplanation",
     async () => {
-      // explanationProvider.addExplanation();
-
-      vscode.window.showInputBox({ prompt: "Did this work?" });
-
       vscode.window.withProgress(
         {
           title: "Generating Explanation...",
           location: vscode.ProgressLocation.Notification,
           cancellable: true,
         },
-        (progress, token) => {
-          return generateExplanation();
+        async (progress, token) => {
+          const selection = vscode.window.activeTextEditor?.selection;
+          if (
+            selection?.start &&
+            selection.end &&
+            vscode.window.activeTextEditor
+          ) {
+            const content = vscode.window.activeTextEditor?.document.getText(
+              new vscode.Range(selection.start, selection?.end)
+            );
+
+            if (!provider.view) {
+              await vscode.commands.executeCommand(
+                "generate-explanation.explanation.focus"
+              );
+            } else {
+              provider.view?.show(true);
+            }
+
+            const result = await new GenerateExplanationCommand().ExecuteAsync({
+              Content: content,
+              OpenAiClient: openai,
+              Provider: provider,
+            });
+
+            if (provider.view) {
+              provider.view.webview.html = result;
+            } else {
+              vscode.window.showInformationMessage(
+                "please select the code you would like to comment."
+              );
+            }
+          }
         }
       );
     }
   );
 
-  let addExp = vscode.commands.registerCommand("firstextension.addexp", () => {
-    explanationProvider.addExplanation();
-  });
-
-  let autoCommentCmd = vscode.commands.registerCommand(
+  let generateComment = vscode.commands.registerCommand(
     "firstextension.generatecomment",
     async () => {
       vscode.window.withProgress(
@@ -68,119 +86,55 @@ export function activate(context: vscode.ExtensionContext) {
           location: vscode.ProgressLocation.Notification,
           cancellable: true,
         },
-        (progress, token) => {
-          return generateComment();
+        async (progress, token) => {
+          const selection = vscode.window.activeTextEditor?.selection;
+          if (
+            selection?.start &&
+            selection.end &&
+            vscode.window.activeTextEditor
+          ) {
+            const content = vscode.window.activeTextEditor?.document.getText(
+              new vscode.Range(selection.start, selection?.end)
+            );
+            const startPos = vscode.window.activeTextEditor?.document.lineAt(
+              vscode.window.activeTextEditor?.selection.start.line
+            ).range.start;
+            const indentation = vscode.window.activeTextEditor?.document.lineAt(
+              startPos.line
+            ).firstNonWhitespaceCharacterIndex;
+
+            const comment = await new GenerateCommentCommand().ExecuteAsync({
+              Content: content,
+              Indentation: indentation,
+              OpenAiClient: openai,
+            });
+
+            if (comment) {
+              vscode.window.activeTextEditor.edit((editBuilder) => {
+                editBuilder.insert(startPos, comment);
+              });
+            }
+          } else {
+            vscode.window.showInformationMessage(
+              "please select the code you would like to comment."
+            );
+          }
         }
       );
     }
   );
 
   context.subscriptions.push(
-    autoCommentCmd,
-    autoExplanation,
+    generateComment,
+    generateExplanation,
     vscode.window.registerWebviewViewProvider(
       ExplanationViewProvider.viewType,
-      explanationProvider,
+      provider,
       {
         webviewOptions: { retainContextWhenHidden: true },
       }
-    ),
-    addExp
+    )
   );
-}
-
-class ExplanationViewProvider implements vscode.WebviewViewProvider {
-  public static readonly viewType = "generate-explanation.explanation";
-
-  private _view?: vscode.WebviewView;
-  private _response?: string;
-  private _promtp?: string;
-
-  constructor(private readonly _extensionUri: vscode.Uri) {}
-
-  resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    context: vscode.WebviewViewResolveContext,
-    token: vscode.CancellationToken
-  ) {
-    this._view = webviewView;
-
-    webviewView.webview.options = {
-      // Allow scripts in the webview
-      enableScripts: true,
-      localResourceRoots: [this._extensionUri],
-    };
-
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-
-    webviewView.webview.onDidReceiveMessage((data) => {
-      let code = data.value;
-      const snippet = new vscode.SnippetString();
-      snippet.appendText(code);
-      // insert the code as a snippet into the active text editor
-      vscode.window.activeTextEditor?.insertSnippet(snippet);
-    });
-  }
-
-  public addExplanation() {
-    if (this._view) {
-      this._view?.show?.(true);
-      this._view?.webview.postMessage("generateExplanation");
-    }
-  }
-
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-    const scriptUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
-    );
-
-    const styleResetUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
-    );
-    const styleVSCodeUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
-    );
-    const styleMainUri = webview.asWebviewUri(
-      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
-    );
-
-    // Use a nonce to only allow a specific script to be run.
-    const nonce = getNonce();
-
-    return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-
-      <!--
-        Use a content security policy to only allow loading styles from our extension directory,
-        and only allow scripts that have a specific nonce.
-        (See the 'webview-sample' extension sample for img-src content security policy examples)
-      -->
-      <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
-
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-      <link href="${styleResetUri}" rel="stylesheet">
-      <link href="${styleVSCodeUri}" rel="stylesheet">
-      <link href="${styleMainUri}" rel="stylesheet">
-
-      <title>Cat Colors</title>
-    </head>
-    <body>
-    
-      <div>
-        <h1>This is for explaining code</h1>
-        <button class="add-color-button">Add Color</button>
-      </div>
-
-      
-
-      <script nonce="${nonce}" src="${scriptUri}"></script>
-    </body>
-    </html>`;
-  }
 }
 
 // This method is called when your extension is deactivated
---
+++ src/services/viewProviders/ExplanationViewProvider.ts
@@ -0,0 +1,125 @@
+import * as vscode from "vscode";
+import { getNonce } from "../../getNonce";
+import { Configuration, OpenAIApi } from "openai";
+
+export class ExplanationViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewType = "generate-explanation.explanation";
+
+  public view?: vscode.WebviewView;
+
+  private configuration = new Configuration({
+    organization: "org-dEtIga6fE6tm2FkqJmKYh9Qc",
+    apiKey: "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",
+  });
+  private openai = new OpenAIApi(this.configuration);
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext,
+    token: vscode.CancellationToken
+  ) {
+    this.view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+  }
+
+  public async addExplanation() {
+    if (!this.view) {
+      await vscode.commands.executeCommand(
+        "generate-explanation.explanation.focus"
+      );
+    } else {
+      this.view?.show?.(true);
+    }
+
+    // Get the selected text of the active editor
+    const selection = vscode.window.activeTextEditor?.selection;
+    // If a user does not want to append this information to their prompt, leave it as an empty string
+
+    if (selection?.start && selection.end) {
+      const content = vscode.window.activeTextEditor?.document.getText(
+        new vscode.Range(selection.start, selection?.end)
+      );
+
+      const result = await this.openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "user",
+            content: `Please provide an explanation for the following code, but keep it succint and readable to a non programmer and try to avoid using programming terms: ${content}`,
+          },
+        ],
+        temperature: 0.7,
+      });
+
+      if (this.view) {
+        this.view.webview.html = result.data.choices[0].message?.content ?? "";
+      }
+    } else {
+      vscode.window.showInformationMessage(
+        "please select the code you would like to comment."
+      );
+    }
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
+
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+  
+        <!--
+          Use a content security policy to only allow loading styles from our extension directory,
+          and only allow scripts that have a specific nonce.
+          (See the 'webview-sample' extension sample for img-src content security policy examples)
+        -->
+        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+        <link href="${styleResetUri}" rel="stylesheet">
+        <link href="${styleVSCodeUri}" rel="stylesheet">
+        <link href="${styleMainUri}" rel="stylesheet">
+  
+        <title>Cat Colors</title>
+      </head>
+      <body>
+      
+        <div>
+          <h3>Ctrl + Shift + f12 to generate an explanation of your selected code</h3>
+          <div class="content"></div>
+        </div>
+  
+        
+  
+        <script nonce="${nonce}" src="${scriptUri}"></script>
+      </body>
+      </html>`;
+  }
+}
