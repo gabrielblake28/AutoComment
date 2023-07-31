22bea244773e96195a82fb43fafe02e415eed1b6
Caleb Blake <cdblake31@gmail.com>
2023-07-30 21:45:14 -0700

    Wip

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import AutoExplanationProvider from "./services/AutoExplanation/AutoExplanationC
 import AutoCommentCommandProvider from "./services/AutoComment/AutoCommentCommandProvider";
 import { openAI } from "./config/openAI";
 import { FirebaseAuthProvider } from "./services/AuthService/FirebaseAuthProvider";
-
+import { AuthenticationService } from "./services/AuthService/AuthService";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -15,24 +15,64 @@ import { FirebaseAuthProvider } from "./services/AuthService/FirebaseAuthProvide
  * @returns None
  */
 export function activate(context: vscode.ExtensionContext) {
+  vscode.window.registerUriHandler({
+    async handleUri(uri: vscode.Uri) {
+      if (uri.path == "/auth_complete") {
+        var params = new URLSearchParams(uri.query);
 
+        const accessToken = params.get("code");
+        if (accessToken) {
+          var session = await new AuthenticationService(
+            context,
+            firebaseAuthProvider
+          ).TryAuthenticateSession(accessToken);
+
+          if (session) {
+            vscode.window.showInformationMessage("Sign in was successful");
+            return;
+          }
+        }
+      }
+
+      vscode.window.showErrorMessage(
+        "Sign in was unsuccessful. Please try again. If your problem persists, contact us at support@codesenseai.com"
+      );
+    },
+  });
+
+  console.log(context.extension.extensionUri, context.extension.id);
   const extensionConfiguration = new ExtensionConfiguration();
   extensionConfiguration.Initialize();
 
-  const explanationViewProvider = new ExplanationViewProvider(openAI,context.extensionUri);
-  const firebaseAuthProvider = new FirebaseAuthProvider(context, extensionConfiguration);
+  const explanationViewProvider = new ExplanationViewProvider(
+    openAI,
+    context.extensionUri
+  );
+  const firebaseAuthProvider = new FirebaseAuthProvider(
+    context,
+    extensionConfiguration
+  );
 
   context.subscriptions.push(
-    new AutoExplanationProvider(openAI, context, firebaseAuthProvider, explanationViewProvider).RegisterCommand(),
-    new AutoCommentCommandProvider(openAI, context, firebaseAuthProvider).RegisterCommand(),
+    new AutoExplanationProvider(
+      openAI,
+      context,
+      firebaseAuthProvider,
+      explanationViewProvider
+    ).RegisterCommand(),
+    new AutoCommentCommandProvider(
+      openAI,
+      context,
+      firebaseAuthProvider
+    ).RegisterCommand(),
     vscode.window.registerWebviewViewProvider(
       ExplanationViewProvider.viewType,
       explanationViewProvider,
       {
         webviewOptions: { retainContextWhenHidden: true },
       }
-    )  
-    );
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
--- src/services/Command/CommandProviderBase.ts
+++ src/services/Command/CommandProviderBase.ts
@@ -5,53 +5,66 @@ import { FirebaseAuthProvider } from "../AuthService/FirebaseAuthProvider";
 import { SubscriptionPlanTier as SubscriptionPlanTier } from "../AuthService/SubscriptionPlanTier";
 
 export abstract class CommandProviderBase {
-    protected readonly AuthService: AuthenticationService;    
+  protected readonly AuthService: AuthenticationService;
 
-    constructor(
-        protected readonly context: vscode.ExtensionContext, 
-        authProvider: FirebaseAuthProvider) {
-        this.AuthService = new AuthenticationService(context, authProvider);
+  constructor(
+    protected readonly context: vscode.ExtensionContext,
+    authProvider: FirebaseAuthProvider
+  ) {
+    this.AuthService = new AuthenticationService(context, authProvider);
+  }
+
+  protected abstract RegisterCommandBase(
+    commandOptions: CommandOptions
+  ): vscode.Disposable;
+
+  protected async executeCommand(
+    command: IVscodeCommand,
+    commandOptions: CommandOptions
+  ): Promise<Thenable<boolean>> {
+    if (commandOptions.UseAuthentication) {
+      var session = await this.AuthService.GetAuthenticatedSession();
+      if (!session) {
+        vscode.window.showErrorMessage("Please login to access services.");
+        return false;
+      }
+
+      if (
+        !this.AuthService.TryAuthorizeCommand(
+          [...session.scopes],
+          commandOptions.UseAuthentication
+        )
+      ) {
+        vscode.window.showErrorMessage(
+          "Your current plan does not support this command. Upgrade at https://codesenseai.com"
+        );
+
+        if (!this.context.globalState.get("firstextension.authredirect")) {
+          this.context.globalState.update(`firstextension.authredirect`, true);
+          vscode.env.openExternal(
+            vscode.Uri.parse("https://codesenseai.com/#pricing")
+          );
+        }
+        return false;
+      }
     }
 
-    protected abstract RegisterCommandBase(commandOptions: CommandOptions): vscode.Disposable;
-
-    protected async executeCommand(command: IVscodeCommand, commandOptions: CommandOptions): Promise<Thenable<boolean>> {
-        if(commandOptions.UseAuthentication) {
-            var session = await this.AuthService.TryAuthenticate();
-            if(!session) {
-                vscode.window.showErrorMessage("Please login to access services.");
-                return false;
-            }
-
-            if(!this.AuthService.TryAuthorizeCommand([...session.scopes], commandOptions.UseAuthentication)) {
-                vscode.window.showErrorMessage("Your current plan does not support this command. Upgrade at https://codesenseai.com");
-
-                if(!this.context.globalState.get("firstextension.authredirect")) {
-                    this.context.globalState.update(`firstextension.authredirect`, true);
-                    vscode.env.openExternal(vscode.Uri.parse("https://codesenseai.com/#pricing"));
-                }
-
-                return false;
-            }   
+    if (commandOptions.UseProgress) {
+      return vscode.window.withProgress(
+        commandOptions.UseProgress,
+        (_, token) => {
+          return command.ExecuteCommand(token);
         }
-
-        if(commandOptions.UseProgress) {
-            return vscode.window.withProgress(
-                commandOptions.UseProgress,
-                (_, token) => {
-                    return command.ExecuteCommand(token);
-                }
-            )
-        }
-
-        return command.ExecuteCommand();
+      );
     }
+
+    return command.ExecuteCommand();
+  }
 }
 
 export interface CommandOptions {
-    UseAuthentication?: SubscriptionPlanTier,
-    UseProgress?: CommandProgressOptions,
+  UseAuthentication?: SubscriptionPlanTier;
+  UseProgress?: CommandProgressOptions;
 }
 
-export interface CommandProgressOptions extends vscode.ProgressOptions {};
-
+export interface CommandProgressOptions extends vscode.ProgressOptions {}
